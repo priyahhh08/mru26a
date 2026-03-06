c9fe726aad6e29721436cff5671ae58f85debab2
priyahhh08 <priyavadlakonda04@gmail.com>
2026-03-06 13:42:48 +0530

    Add files via upload

---
+++ part4/page34.js
@@ -0,0 +1,46 @@
+
+import express from "express";
+const app = express();
+app.set("view engine", "ejs");
+app.set("views", "views");
+app.listen(5000, () => console.log("Server Started"));
+
+app.use(express.urlencoded({ extended: true }));
+
+let users = [
+  { name: "Priya", email: "priya@gmail.com", password: "1234" },
+  { name: "madhu", email: "madhu@gmail.com", password: "1234" },
+  { name: "likki", email: "likki@gmail.com", password: "1234" },
+];
+
+app.get("/login", (req, res) => {
+  res.render("login", { error: null });
+});
+
+app.post("/login", (req, res) => {
+  const { email, password } = req.body;
+  const user = users.find((user) => user.email === email);
+  if (user) {
+    if (user.password === password) {
+      res.redirect("/");
+    } else {
+      res.render("login", { error: "Invalid Password" });
+    }
+  } else {
+    res.render("login", { error: "User not found" });
+  }
+  // res.redirect("/");
+});
+
+app.get("/register", (req, res) => {
+  res.render("register");
+});
+
+app.post("/register", (req, res) => {
+  users = [...users, req.body];
+  res.redirect("/");
+});
+
+app.get("/", (req, res) => {
+  res.render("dashboard", { users });
+});
